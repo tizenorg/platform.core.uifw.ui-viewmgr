73096ba0c95d12a5d6d3ffcc4123b031279b4881
Woochan Lee <wc0917.lee@samsung.com>
2016-03-22 11:41:59 +0900

    Add rotated method in controller.
    Change-Id: Id136fc79a8a15b086f3dbff41f8fa861b7e375c6

--- src/lib/efl/ui_viewmgr.cpp
+++ src/lib/efl/ui_viewmgr.cpp
@@ -171,6 +171,19 @@ bool ui_viewmgr::create_conformant(Evas_Object *win)
 	return true;
 }
 
+static void rotation_cb(void *data, Evas_Object *obj, void *event_info)
+{
+	int rot = elm_win_rotation_get(obj);
+
+	ui_viewmgr *viewmgr = static_cast<ui_viewmgr *>(data);
+	ui_view *view = viewmgr->get_last_view();
+	view->rotate(rot);
+
+	//FIXME: Change this configurable?
+	if (rot == 0 || rot == 180) view->portrait();
+	else view->landscape();
+}
+
 ui_viewmgr::ui_viewmgr(const char *pkg, ui_key_listener *key_listener)
 		: ui_iface_viewmgr(), key_listener(key_listener), transition_style("default")
 {
@@ -188,6 +201,7 @@ ui_viewmgr::ui_viewmgr(const char *pkg, ui_key_listener *key_listener)
 		return;
 	}
 
+	//FIXME: Make a method? to set available rotation degree.
 	//Set window rotation
 	if (elm_win_wm_rotation_supported_get(this->win))
 	{
@@ -195,6 +209,7 @@ ui_viewmgr::ui_viewmgr(const char *pkg, ui_key_listener *key_listener)
 		{ 0, 90, 180, 270 };
 		elm_win_wm_rotation_available_rotations_set(this->win, (const int *) (&rots), 4);
 	}
+	evas_object_smart_callback_add(this->win, "wm,rotation,changed", rotation_cb, this);
 
 	//Window is requested to delete.
 	evas_object_smart_callback_add(this->win, "delete,request",
