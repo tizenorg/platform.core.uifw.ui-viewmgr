feaada322a64ed45aa6be6b9baf999a3264fe767
Hermet Park <hermet@hermet.pe.kr>
2016-05-02 17:41:09 +0900

    sort out public/internal apis.
    Change-Id: Iad050a5570e6119fd325994d84b26e22e597019b
    target: ui_iface_overlay.

--- src/lib/interface/ui_iface_overlay.cpp
+++ src/lib/interface/ui_iface_overlay.cpp
@@ -17,39 +17,116 @@
 
 #include "../../include/interface/ui_iface_viewmanager.h"
 
-ui_iface_overlay::ui_iface_overlay(ui_iface_view *view)
-		: view(view), content(NULL)
+
+/***********************************************************************************************/
+/* Internal class Implementation                                                               */
+/***********************************************************************************************/
+namespace ui_viewmanager
+{
+
+class ui_iface_overlay_impl
+{
+	friend class ui_iface_overlay;
+
+private:
+	ui_iface_overlay *overlay;
+	ui_iface_view *view;
+	T content;
+
+public:
+	bool set_content(T content);
+	T unset_content();
+	ui_iface_view *get_view();
+	T get_content();
+
+	ui_iface_overlay_impl(ui_iface_overlay *overlay, ui_iface_view *view);
+	~ui_iface_overlay_impl();
+};
+
+}
+
+ui_iface_overlay_impl::ui_iface_overlay_impl(ui_iface_overlay *overlay, ui_iface_view *view)
+		: overlay(overlay), view(view), content(NULL)
 {
 }
 
-ui_iface_overlay::~ui_iface_overlay()
+ui_iface_overlay_impl::~ui_iface_overlay_impl()
 {
 }
 
-bool ui_iface_overlay::set_content(T content)
+bool ui_iface_overlay_impl::set_content(T content)
 {
 	this->content = content;
 	return true;
 }
 
-T ui_iface_overlay::unset_content()
+T ui_iface_overlay_impl::unset_content()
 {
 	T prev = this->content;
 	this->content = NULL;
 	return prev;
 }
 
-T ui_iface_overlay::get_content()
+T ui_iface_overlay_impl::get_content()
 {
 	return this->content;
 }
 
-ui_iface_view *ui_iface_overlay::get_view()
+ui_iface_view *ui_iface_overlay_impl::get_view()
 {
 	return this->view;
+}
+
+
+/***********************************************************************************************/
+/* External class Implementation                                                               */
+/***********************************************************************************************/
+ui_iface_overlay::ui_iface_overlay(ui_iface_view *view)
+{
+	this->impl = new ui_iface_overlay_impl(this, view);
+}
+
+ui_iface_overlay::~ui_iface_overlay()
+{
+	delete(this->impl);
+}
+
+bool ui_iface_overlay::set_content(T content)
+{
+	return this->impl->set_content(content);
+}
+
+T ui_iface_overlay::unset_content()
+{
+	return this->impl->unset_content();
+}
+
+T ui_iface_overlay::get_content()
+{
+	return this->impl->get_content();
+}
+
+ui_iface_view *ui_iface_overlay::get_view()
+{
+	return this->impl->get_view();
 }
 
 void ui_iface_overlay::on_back()
 {
 	this->deactivate();
+}
+
+bool ui_iface_overlay::activate()
+{
+	return true;
+}
+
+bool ui_iface_overlay::deactivate()
+{
+	return true;
+}
+
+bool ui_iface_overlay::is_activated()
+{
+	return true;
 }
