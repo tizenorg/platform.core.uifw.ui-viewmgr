015c398bd9f94ce34d6380965a8c5fa060dc6ec0
Hermet Park <hermet@hermet.pe.kr>
2016-06-09 17:22:59 +0900

    c++: updated doc.
    Change-Id: Iaf86948e357242e505804c69b524ed9d975e03bc

--- src/include/efl/mobile/ui_viewmgr.h
+++ src/include/efl/mobile/ui_viewmgr.h
@@ -28,9 +28,8 @@ class ui_app;
  *
  *  @ingroup efl_viewmanager
  *
- *  @brief This is a mobile EFL view manager class. Typically, this view manager extends ui_base_viewmgr and implements mobile specific behaviors for
- *         EFL view manager in mobile profile. ui_viewmgr is nothing more than ui_base_viewmgr in behavior perspective. It just comes out with renaming class
- *         for adapting with other EFL mobile classes.
+ *  @brief This is a mobile ui application class. Typically, this application extends ui_iface_app and implements mobile specific behaviors for mobile profile.
+ *         ui_app is nothing more than ui_iface_app in behavior perspective. It just comes out with renaming class for adapting with other EFL mobile classes.
  */
 class ui_viewmgr: public ui_base_viewmgr
 {
